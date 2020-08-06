860ff6ad3c770530ba7141ea6f7436e063d026bb
RobCM <robert.coleman.martinez@gmail.com>
2020-08-06 17:14:34 -0500

    Create css_template.php
    template code for css stuff

---
+++ includes/css_template.php
@@ -0,0 +1,20 @@
+<?php
+	/*
+		CSS__template
+		
+		@RobertC 8/6/2020
+		
+		function to pass css code
+
+	*/
+	function css_template(){
+		echo '
+			<style>
+				body {
+				  background-color: #ad6d68;
+				}
+
+			</style>
+		';
+	}
+?>
