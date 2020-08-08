7e4c0336453376111b4ccb44b0fc29980402c3d4
RobCM <robert.coleman.martinez@gmail.com>
2020-08-08 11:15:18 -0500

    Update index.php

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
 	include './includes/html_footer.php';
 	include './includes/css_template.php';
 	include './includes/js_template.php';
+	include './db/db_connection.php';
 	
 	// Set up header and page title
 	html_header("Test Page");
@@ -15,7 +16,11 @@
 	
 	// Example body!
 	echo '
-			<br><h4>Hi there this project is still under construction</h4><br>;) started on 8/4/2020';
+			<br><h4>Hi there this project is still under construction</h4><br>;) started on 8/4/2020 <br>';
+			
+	
+	// Testing db function (MSSQL)!
+	conn_mssql();
 	
 	// Set up footer and text on footer
 	html_footer("2020 test page!");
