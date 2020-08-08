61da224faa5d33bfdfee3268602f78f79d59324f
RobCM <robert.coleman.martinez@gmail.com>
2020-08-08 10:43:46 -0500

    Create db_connection.php
    Adding database structure folder.

---
+++ db/db_connection.php
@@ -0,0 +1,27 @@
+<?php
+	/*
+		DB__connection
+		
+		@RobertC 8/8/2020
+		
+		functions for database connection-------------
+		Depending what type of database you will end up
+		using is the function you will call/ use.
+		----------------------------------------------
+		
+		Database's suported so far MySQL and Microsoft SQL Server
+		
+
+	*/
+	
+	// For MySQL....
+	function conn_mysql(){
+		
+	}
+	
+	// For MSSQL....
+	function conn_mssql(){
+		
+	}
+	
+?>
