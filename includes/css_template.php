7343bd909eaf7e2f4fed55f000de7483e6c6d26d
RobCM <robert.coleman.martinez@gmail.com>
2020-08-17 17:07:41 -0500

    Update css_template.php

--- includes/css_template.php
+++ includes/css_template.php
@@ -5,15 +5,34 @@
 		@RobertC 8/6/2020
 		
 		function to pass css code
+		
+		Using display flex which will put the div's inside of the master container	
+		next to each other without the need of a table structure!
 
 	*/
 	function css_template(){
 		echo '
 			<style>
 				body {
-				  background-color: #ad6d68;
+					background-color: #bdf4f2;
 				}
-
+				.master_container {
+					display: flex;
+					width: 50%;
+					margin: auto;
+					border: 1px solid black;
+					padding: 10px;
+				}
+				#container_1 {
+					flex: 1;
+					border: 1px solid black;
+					padding: 5px;
+				}
+				#container_2 {
+					flex: 2;
+					border: 1px solid black;
+					padding: 5px;
+				}
 			</style>
 		';
 	}
