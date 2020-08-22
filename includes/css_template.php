a628335995c8831150f57de6adbd8259e49664b0
RobCM <robert.coleman.martinez@gmail.com>
2020-08-22 15:03:27 -0500

    Update css_template.php

--- includes/css_template.php
+++ includes/css_template.php
@@ -19,18 +19,19 @@
 				.master_container {
 					display: flex;
 					width: 50%;
+					height: 400px;
 					margin: auto;
 					border: 1px solid black;
 					padding: 10px;
+					border-radius: 15px;
+					box-shadow: 9px 14px 6px 0px rgba(0,0,0,0.38);
 				}
 				#container_1 {
 					flex: 1;
-					border: 1px solid black;
 					padding: 5px;
 				}
 				#container_2 {
 					flex: 2;
-					border: 1px solid black;
 					padding: 5px;
 				}
 			</style>
