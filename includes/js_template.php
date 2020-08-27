e048efa9d1c5041e7017f4f943a722651ece3cbe
RobCM <robert.coleman.martinez@gmail.com>
2020-08-27 18:36:24 -0500

    Update js_template.php

--- includes/js_template.php
+++ includes/js_template.php
@@ -5,11 +5,17 @@
 		@RobertC 8/5/2020
 		
 		function to pass JavaScript code
+		
+		It also calls the jQuery library
 
 	*/
 	function js_template(){
 		echo '
+			<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 			<script>
+				// Basic animation 
+				$("form").hide();
+				$("form").fadeIn(2000);
 			</script>
 		';
 	}
