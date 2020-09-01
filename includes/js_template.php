66c48076b51c488e0053d8bc793cc4193d17e900
RobCM <robert.coleman.martinez@gmail.com>
2020-09-01 18:55:26 -0500

    Update js_template.php

--- includes/js_template.php
+++ includes/js_template.php
@@ -9,13 +9,44 @@
 		It also calls the jQuery library
 
 	*/
-	function js_template(){
+	function js_gui(){
 		echo '
 			<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 			<script>
+			
 				// Basic animation 
 				$("form").hide();
 				$("form").fadeIn(2000);
+				
+			</script>
+		';
+	}
+	
+	function js_ajax(){
+		echo'
+			<script>
+			
+			// Ajax 
+				var url_value = "/post_url.php";
+				
+				$.ajax({
+					url: url_value,
+					type: "POST",
+					data: {
+						value_one: "test_one",
+						value_two: "test_two"
+					},
+					dataType: "json",
+					success: function (data){
+						// Append to HTML element
+						$("container_1").append(data);
+					},
+					error: function (error){
+						// Append to HTML element
+						$("container_1").append("Error ${error}");
+					}
+				});
+			
 			</script>
 		';
 	}
