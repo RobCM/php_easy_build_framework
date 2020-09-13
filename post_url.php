a92c7f5402a3ffafa2b4c5aca95246acc7b01378
RobCM <robert.coleman.martinez@gmail.com>
2020-09-13 08:49:23 -0500

    Update post_url.php

--- post_url.php
+++ post_url.php
@@ -9,10 +9,11 @@
 
 	*/
 	
-	// Parameters past by the AJAX using mysqli_real_escape_string to prevent SQL INJECTION!
-	$value_one = mysqli_real_escape_string($_POST['value_one']);
-	$value_two = mysqli_real_escape_string($_POST['value_two']);
+	// To block PHP errors comment out if need it for testing!
+	error_reporting(E_ERROR);
 	
+	// Array for JSON data to be return
+	$json_response = [];
 	
 	$server_name = "localhost"; // Replace with your server name!
 	$user_name = "databaseuser"; 
@@ -22,6 +23,10 @@
 	// Check on the connection!
 	echo (!$conn->connect_error) ? 'Database connected!' : 'Error! no DB connection';
 	
+	// Parameters past by the AJAX using mysqli_real_escape_string to prevent SQL INJECTION!
+	$value_one = mysqli_real_escape_string($conn, $_POST['value_one']);
+	$value_two = mysqli_real_escape_string($conn, $_POST['value_two']);
+	
 	// SQL query
 	$sql = 'SELECT * FROM table_name;';
 	$result = mysqli_query($conn, $sql) or die(mysqli_error($conn));
@@ -30,15 +35,18 @@
 		
 		// For each row of data!
 		while($row = mysqli_fetch_assoc($result)){
-			echo $row['data_item_colum'];
+			$data_1 =  $row['data_item_colum'];
+			
+			array_push($json_response, $data_1);
 		}
 		
 	}
 	else{
-		echo 'No data!';
+		array_push($json_response, 'No results');
 	}
 	
-	// Retrun result as JSON <---- work in progress!
-	
+	// Convert data to JSOn format
+	$json_data = json_encode($json_response);
+	echo $json_data;	
 	
 ?>
