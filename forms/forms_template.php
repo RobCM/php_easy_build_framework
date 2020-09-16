8b04845f44ae340eecfcc8e96e46848d58c4a315
RobCM <robert.coleman.martinez@gmail.com>
2020-09-16 17:58:38 -0500

    Update forms_template.php

--- forms/forms_template.php
+++ forms/forms_template.php
@@ -13,13 +13,13 @@
 		// Form HTML
 		$form_1 = '
 				<form>
-				  <label for="first_name">First name:</label><br>
-				  <input type="text" id="first_name" name="first_name"><br><br>
-				  <label for="last_name">Last name:</label><br>
-				  <input type="text" id="last_name" name="last_name"><br><br>
-				  <label for="last_name">Password:</label><br>
-				  <input type="password" id="user_pass" name="user_pass"><br><br>
-				  <button type="button" id="submit_btn">Submit</button> 
+					<label for="first_name">First name:</label><br>
+					<input type="text" id="first_name" name="first_name"><br><br>
+					<label for="last_name">Last name:</label><br>
+					<input type="text" id="last_name" name="last_name"><br><br>
+					<label for="last_name">Password:</label><br>
+					<input type="password" id="user_pass" name="user_pass"><br><br>
+					<button type="button" id="submit_btn">Submit</button> 
 				</form> 
 		';
 		
@@ -32,19 +32,36 @@
 		// Form HTML
 		$form_2 = '
 				<form>
-				  <label for="first_name">First name:</label><br>
-				  <input type="text" id="first_name" name="first_name"><br><br>
-				  <label for="last_name">Last name:</label><br>
-				  <input type="text" id="last_name" name="last_name"><br><br>
-				  <label for="phone">Phone number:</label><br>
-				  <input type="text" id="phone" name="phone"><br><br>
-				  <label for="last_name">Password:</label><br>
-				  <input type="password" id="user_pass" name="user_pass"><br><br>
-				  <button type="button" id="submit_btn">Submit</button> 
+					<label for="first_name">First name:</label><br>
+					<input type="text" id="first_name" name="first_name"><br><br>
+					<label for="last_name">Last name:</label><br>
+					<input type="text" id="last_name" name="last_name"><br><br>
+					<label for="phone">Phone number:</label><br>
+					<input type="text" id="phone" name="phone"><br><br>
+					<label for="last_name">Password:</label><br>
+					<input type="password" id="user_pass" name="user_pass"><br><br>
+					<button type="button" id="submit_btn">Submit</button> 
 				</form> 
 		';
 		
 		return $form_2;
+		
+	}
+	
+	function form_3(){
+		
+		// Form HTML
+		$form_3 = '
+				<form>		
+					<label for="user_id">User ID:</label>	
+					<input type="password" id="user_id" name="user_id"><br><br>
+					<label for="last_name">Password:</label>
+					<input type="password" id="user_pass" name="user_pass"><br><br>
+					<button type="button" id="submit_btn">Submit</button> 				
+				</form> 
+		';
+		
+		return $form_3;
 		
 	}
 	
