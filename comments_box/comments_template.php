3217965021685361b2e4709f7f901f5376af0f98
RobCM <robert.coleman.martinez@gmail.com>
2020-11-19 17:29:37 -0600

    Create comments_template.php

---
+++ comments_box/comments_template.php
@@ -0,0 +1,27 @@
+<?php
+	/*
+		COMMENTS__template
+		
+		@RobertC 11/16/2020
+		
+		functions to pass comments templates code
+
+	*/
+	
+	function comments_1(){
+		
+		// HTML
+		$comments_1 = '
+				<div id="main_comment_container">
+					<div id="text_comment_container">
+						<textarea id="text_comment" rows="10" cols="60">
+						</textarea>
+					</div>
+				</div>
+		';
+		
+		return $comments_1;
+		
+	}
+	
+?>
