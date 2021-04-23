18e3155e68c8dc9cf0f8218c81bb1633749fcf8e
RobCM <robert.coleman.martinez@gmail.com>
2021-04-22 19:14:31 -0500

    Create post_test_dynamic.php

---
+++ post_test_dynamic.php
@@ -0,0 +1,17 @@
+<?php
+	/*
+		post__test_dynamic
+		
+		@RobertC 4/22/2020
+		
+		This is a test php to be use by the JS AJAX script!
+		
+
+	*/
+	
+	
+	// Convert data to JSON format
+	$json_data = json_encode($json_response);
+	echo $json_data;	
+	
+?>
