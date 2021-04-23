bc74180949eb9ff01a14029ee0dd3fbcede094db
RobCM <robert.coleman.martinez@gmail.com>
2021-04-23 18:36:27 -0500

    Update post_test_dynamic.php

--- post_test_dynamic.php
+++ post_test_dynamic.php
@@ -5,10 +5,24 @@
 		@RobertC 4/22/2020
 		
 		This is a test php to be use by the JS AJAX script!
-		
-
+		To test the dynamic "tables" to be display on GUI.
+		Normally your DB SQL or stored procedure call will 
+		go here and fill up the json response array!
 	*/
 	
+	
+	// To block PHP errors comment out if need it for testing!
+	error_reporting(E_ERROR);
+	
+	// Array for JSON data to be return
+	$json_response = [];
+	
+	
+    $json_response = array(
+		  array("first_name"=>"Thomas", "last_name"=>"Richerdson", "age"=>45),
+		  array("first_name"=>"Amanda", "last_name"=>"Ramirez", "age"=>31),
+		  array("first_name"=>"Joe", "last_name"=>"Dunn", "age"=>2)
+      );
 	
 	// Convert data to JSON format
 	$json_data = json_encode($json_response);
