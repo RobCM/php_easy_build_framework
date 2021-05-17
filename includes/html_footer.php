5df373fb856a23d37f0eb28231a45c4ef8bc9cd1
RobCM <robert.coleman.martinez@gmail.com>
2021-05-17 17:36:13 -0500

    Update html_footer.php

--- includes/html_footer.php
+++ includes/html_footer.php
@@ -7,14 +7,18 @@
 		pass the text as a parameter on this function!
 
 	*/
-	function html_footer($page_footer_text){
-		echo '
-					<div class="page_footer_box">
-						<center>'.$page_footer_text.'</center>
-					</div>
-				</body>
-			</html>
-			
-		';
+	class easy_php_footer{
+		
+		public function html_footer($page_footer_text){
+			echo '
+						<div class="page_footer_box">
+							<center>'.$page_footer_text.'</center>
+						</div>
+					</body>
+				</html>
+				
+			';
+		}
+		
 	}
 ?>
