1f6575d042b750502235d67dc119ab719c0c61dc
RobCM <robert.coleman.martinez@gmail.com>
2022-06-08 17:44:14 -0500

    Update comments_template.php

--- comments_box/comments_template.php
+++ comments_box/comments_template.php
@@ -4,29 +4,33 @@
 		
 		@RobertC 11/16/2020
 		
-		functions to pass comments templates code
+		public functions to pass comments templates code
 
 	*/
 	
-	function comments_1(){
+	class easy_php_form{
 		
-		// HTML
-		$comments_1 = '
-				<div id="main_comment_container">
-					<div id="text_comment_container">
-						<center>Comments</center>
-						<textarea id="text_comment" rows="10" cols="6">
-						</textarea>
-						<button type="button" id="comment_btn" class="parent_btn">Comment</button> 
+		public function comments_1(){
+			
+			// HTML
+			$comments_1 = '
+					<div id="main_comment_container">
+						<div id="text_comment_container">
+							<center>Comments</center>
+							<textarea id="text_comment" rows="10" cols="6">
+							</textarea>
+							<button type="button" id="comment_btn" class="parent_btn">Comment</button> 
+						</div>
+						<br>
+						<div id="history_comment_container">
+						</div>
 					</div>
-					<br>
-					<div id="history_comment_container">
-					</div>
-				</div>
-		';
-		
-		return $comments_1;
-		
+			';
+			
+			return $comments_1;
+			
+		}
+	
 	}
 	
 ?>
